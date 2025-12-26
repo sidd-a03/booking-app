24830e916a51024380b8899aaaa7038203ddf6e2
Achinta <achintasiddhanta0304@gmail.com>
2025-12-27 00:38:44 +0530

    finised navbar and implement clerk

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,12 +1,18 @@
-import {Link, NavLink} from "react-router-dom";
-import {MenuIcon, SearchIcon, XIcon} from "lucide-react";
+import {Link, NavLink, type NavLinkRenderProps, useNavigate} from "react-router-dom";
+import {MenuIcon, SearchIcon, TicketPlus, XIcon} from "lucide-react";
 import {NavItems} from "../constants";
 import type {NavItemInterface} from "../types";
 import {useState} from "react";
+import {useUser, useClerk, UserButton} from "@clerk/clerk-react";
 
 const Navbar = () => {
 
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const { user } = useUser();
+    const { openSignIn } = useClerk();
+    const navigate = useNavigate();
+
+    const navbarStyle = ({isActive}: NavLinkRenderProps): string => `hover:bg-primary-nav hover:rounded-full hover:px-3 hover:py-0.5 transition-all duration-200 ${isActive ? 'text-primary-nav hover:text-white font-semibold' : ''}`
 
     const navlinkHandler = (): void => {
         scrollTo(0,0);
@@ -18,18 +24,29 @@ const Navbar = () => {
             <Link to="/">
                 <img src="/assets/images/logo.png" alt="Logo" className="w-32 h-auto" />
             </Link>
-            <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg flex flex-col md:flex-row items-center max-md:justify-center min-md:px-8 gap-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
+            <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg flex flex-col md:flex-row items-center max-md:justify-center min-md:px-8 gap-8 py-2 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
 
                 <XIcon onClick={() => setIsOpen((prev:boolean): boolean => !prev)} className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" />
 
                 {NavItems.map((item: NavItemInterface)  => (
-                    <NavLink className={({isActive}) => isActive ? 'text-primary-dull' : ''} onClick={navlinkHandler} key={item.id} to={item.url}>{item.title}</NavLink>
+                    <NavLink className={navbarStyle} onClick={navlinkHandler} key={item.id} to={item.url}>{item.title}</NavLink>
                 ))}
 
             </div>
             <div className="flex items-center gap-8">
                 <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
-                <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">Login</button>
+
+                {
+                    !user ? (
+                        <button onClick={() => openSignIn()} className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">Login</button>
+                    ) : (
+                        <UserButton>
+                            <UserButton.MenuItems>
+                                <UserButton.Action label="My Bookings" labelIcon={<TicketPlus width={15} />} onClick={() => navigate('/mybookings')}  />
+                            </UserButton.MenuItems>
+                        </UserButton>
+                    )
+                }
             </div>
             <MenuIcon onClick={() => setIsOpen((prev: boolean): boolean => !prev)} className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" />
         </div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
+import {ClerkProvider} from "@clerk/clerk-react";
+import {dark} from "@clerk/themes";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+      <ClerkProvider
+          publishableKey={PUBLISHABLE_KEY}
+          appearance={{
+              theme: dark
+          }}
+      >
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </ClerkProvider>
   </StrictMode>,
 )
